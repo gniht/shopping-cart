09e5589a68da52f2513e41dbcf0b42fbedc76238
gniht <jasonthomasjat@gmail.com>
2021-10-28 13:32:34 -0700

    added nav bar

---
+++ src/About.js
@@ -0,0 +1,12 @@
+
+
+function About () {
+  return (
+    <div className="about">
+      <h1>About Shop</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex expedita accusamus neque officia voluptates iure dolor exercitationem vel quas maiores, quis quos eligendi natus iusto, dolorem adipisci omnis? Ratione et consequuntur eum totam atque repellendus harum, quo, vero quas aut nulla nemo illum! Error aliquid neque voluptatum asperiores, dignissimos molestias nisi velit ipsum illo provident voluptates cumque ipsam rem sint, dicta veniam. Laudantium, a!</p>    
+    </div>    
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop";
+import About from "./About";
 
 function App() {
   return (
-    <BrowserRouter>      
+    <BrowserRouter>
+      <div className="navBar">
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/about">About</Link>
+      </div>      
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/shop" component={Shop} />        
+        <Route path="/about" component={About} />        
       </Switch>   
     </BrowserRouter>   
   );
