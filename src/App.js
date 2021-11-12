a76a064f7e0e7235a0aa13aa3ba48b1990777c14
gniht <jasonthomasjat@gmail.com>
2021-11-11 21:45:16 -0800

    added dummy items array and started on context for shop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop";
 import About from "./About";
 import Cart from "./Cart";
+import { ShopContext } from './ShopContext';
+import {shopItems} from './shopItems';
 
 function App() {
+  
+  const [ itemsInCart, setItemsInCart ] = useState([]);
   
   return (
     <BrowserRouter>
@@ -13,13 +18,15 @@ function App() {
         <Link className="link" to="/shop">Shop</Link>
         <Link className="link" to="/about">About</Link>
         <Link className="link" to="/cart">Cart</Link>
-      </div>      
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/shop" component={Shop} />        
-        <Route path="/about" component={About} />        
-        <Route path="/cart" component={Cart} />        
-      </Switch>   
+      </div>
+      <ShopContext.Provider value={{ shopItems, itemsInCart, setItemsInCart }}>           
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} /> 
+          <Route path="/cart" component={Cart} />        
+        </Switch>
+      </ShopContext.Provider>
     </BrowserRouter>   
   );
 }
---
+++ src/shopItems.js
@@ -0,0 +1,19 @@
+
+export const shopItems = [
+  {
+    name: 'fork',
+    description: 'a utensil for eating',
+    image: ''
+  },
+  {
+    name: 'spoon',
+    description: 'a utensil for eating liquidy foods',
+    image: ''
+  },
+  {
+    name: 'butter knife',
+    description: 'a utensil for spreading jam, butter, and the like on bread',
+    image: ''
+  }
+];
+
