956aebe06ce0e3e4b75259bc961b8f10e1b1d1fa
gniht <jasonthomasjat@gmail.com>
2021-11-12 18:40:29 -0800

    added number input and price subtotal with proper money formatting

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { ShopContext } from "./ShopContext";
 
-function Item({name, description, price}) {
-  
+function Item({ key, name, description, price}) {
+
+  const { shopItems, itemsInCart, setItemsInCart } = useContext(ShopContext);
+
   const [inCart, setInCart] = useState(false);
   const [quantity, setQuantity] = useState(0);
 
@@ -14,17 +17,27 @@ function Item({name, description, price}) {
       setQuantity(1);
     }
   }
+  function quantityChangeHandler(e){
+    setQuantity(e.target.value)    
+  }
   
   return (
-    <li className="item">
-      {console.log(name)}
-      <h4 className="productName">{name}</h4>
-      {/* <img src={image} alt={description} /> */}
-      <p className="productDescription">{description}</p>
-      <h6>{price}</h6>
-      <button className="btn" onClick={() => addOrRemoveHandler()}>
-        { inCart ? 'Remove Item' : 'Add to Cart'}
-      </button>       
+    <li>
+      <div className="item">
+        <h4 className="productName">{name}</h4>
+        {/* <img src={image} alt={description} /> */}
+        <p className="productDescription">{description}</p>
+        <h6>Price: ${price} each</h6>
+        <div className="btn-controls">
+          <button className="btn" onClick={() => addOrRemoveHandler()}>
+            { inCart ? 'Remove Item' : 'Add to Cart'}
+          </button>
+          { inCart &&  <input type="number" min={1} value={quantity} onChange={(e) => {quantityChangeHandler(e)}}/> }
+          { inCart && <h6 className="subtotal">Subtotal: {`${(price*quantity)
+            .toLocaleString('en-US', {style: 'currency', currency: 'USD' })}`}</h6> } 
+        </div>
+
+      </div>    
     </li>
   );
 }
--- src/shopItems.js
+++ src/shopItems.js
@@ -3,19 +3,19 @@ export const shopItems = [
     name: 'fork',
     description: 'a utensil for eating',
     image: '',
-    price: 3.00
+    price: 2.99
   },
   {
     name: 'spoon',
     description: 'a utensil for eating liquidy foods',
     image: '',
-    price: 2.50
+    price: 2.49
 
   },
   {
     name: 'butter knife',
     description: 'a utensil for spreading jam, butter, and the like on bread',
     image: '',
-    price: 2.00
+    price: 1.99
   }
 ];
